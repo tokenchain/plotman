5f28777dbae51f2a4629481be929eb59f629d0e2
heskdx <jobhesk@gmail.com>
2021-05-09 00:42:37 +0800

    package updates related items. please check in commit details

--- src/plotmanx/Farmplot.py
+++ src/plotmanx/Farmplot.py
@@ -1,6 +1,7 @@
 import datetime
 import glob
 import os
+import time
 from pathlib import Path
 from subprocess import Popen, PIPE, STDOUT
 
@@ -16,6 +17,7 @@ class FarmPlot:
     def __init__(self, cfg: PlotmanConfig):
         self.dsts = cfg.directories.dst
         self.checktmps = cfg.directories.tmp
+        self.schedule = cfg.scheduling.polling_time_s
 
     def isAvailable(self, size) -> (bool, str):
         for d in self.dsts:
@@ -32,33 +34,48 @@ class FarmPlot:
         file_to_rem.unlink()
 
     def checking(self):
-        files_x_list = []
-        for tmp in self.checktmps:
-            path = "%s*.plot".format(tmp)
-            mylist = [f for f in glob.glob(path)]
-            files_x_list.extend(mylist)
+        while True:
+            try:
+                files_x_list = []
+                for tmp in self.checktmps:
+                    path = "%s*.plot".format(tmp)
+                    mylist = [f for f in glob.glob(path)]
+                    files_x_list.extend(mylist)
 
-        if len(files_x_list) > 0:
-            for file in files_x_list:
-                size = Path(file).stat().st_size
-                (a, vDir, lockFile) = self.isAvailable(size)
-                # is busy now?
-                if not a:
-                    continue
+                if len(files_x_list) > 0:
+                    for file in files_x_list:
+                        size = Path(file).stat().st_size
+                        (a, vDir, lockFile) = self.isAvailable(size)
+                        # is busy now?
+                        if not a:
+                            continue
 
-                filename, file_extension = os.path.splitext(os.path.basename(file))
-                source = file
-                destination = os.path.join(vDir, filename, file_extension)
-                t0 = datetime.datetime.now()
-                p = Popen(["mv", "-v", source, destination], stdout=PIPE, stderr=STDOUT)
-                output, error = p.communicate()
-                output = output.strip().decode("utf-8")
-                print("start moving file from temp folder to destination farm location")
-                if p.returncode:
-                    print(f"E: {output}")
-                    self.removeFile(lockFile)
-                else:
-                    elapsed = (datetime.datetime.now() - t0).total_seconds()
-                    self.removeFile(lockFile)
-                    print(output)
+                        filename, file_extension = os.path.splitext(os.path.basename(file))
+                        source = file
+                        destination = os.path.join(vDir, filename, file_extension)
+                        t0 = datetime.datetime.now()
+                        p = Popen(["mv", "-v", source, destination], stdout=PIPE, stderr=STDOUT)
+                        output, error = p.communicate()
+                        output = output.strip().decode("utf-8")
+                        print("start moving file from temp folder to destination farm location")
+                        if p.returncode:
+                            print(f"E: {output}")
+                            self.removeFile(lockFile)
+                        else:
+                            elapsed = (datetime.datetime.now() - t0).total_seconds()
+                            self.removeFile(lockFile)
+                            print(output)
 
+                time.sleep(self.schedule)
+            except ConnectionError as cc:
+                print('got ConnectionError from io', cc)
+                continue
+            except ValueError as v:
+                print('got ValueError from io', v)
+                continue
+            except TimeoutError as th:
+                print('got TimeoutError from io', th)
+                continue
+            except IOError as io:
+                print('got IOError from io', io)
+                continue
