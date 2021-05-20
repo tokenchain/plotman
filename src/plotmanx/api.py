e3c5c4759fef240226c264e01844ca8a61b24105
heskdx <jobhesk@gmail.com>
2021-05-21 02:51:21 +0800

    package updates related items. please check in commit details

--- src/plotmanx/api.py
+++ src/plotmanx/api.py
@@ -12,8 +12,9 @@ from .configuration import PlotmanConfig, get_db_path
 
 
 def PostDat(dp: dict, cfg: PlotmanConfig):
+    print(dp)
     # sending post request and saving response as response object
-    payload = json.loads(json.dumps(dp))
+    payload = json.dumps(dp)
     r = requests.post(url=f'{cfg.apis.target}:{cfg.apis.port}/report', data=payload)
     print(r.text)
 
